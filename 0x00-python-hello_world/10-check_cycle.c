4b2535b8c7c9c6fe972657db14dad5be88c841f4
oberon10 <prosperbernrad@gmail.com>
2023-06-06 13:32:39 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
-* check_cycle -checks if a linked contains a cycle 
+* check_cycle -checks if a linked contains a cycle
 *@list:linked list to cjeck
-*Return: 1 if the list has a cycle, 0 if it doesn't 
+*Return: 1 if the list has a cycle, 0 if it doesn't
 */
-int check_cycle(listint_t*list)
+int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
 	listint_t *fast = list;
 
 	if (!list)
 		return (0);
-	while (slow && fast && fast-> next )
+	while (slow && fast && fast->next)
 	{
 	slow = slow->next;
 	fast = fast->next;
