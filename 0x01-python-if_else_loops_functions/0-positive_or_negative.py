ffb2f2893e8c6d26dac2fad8296bdd058bd4f2e0
oberon10 <prosperbernrad@gmail.com>
2023-06-06 15:22:51 -0700

    0 task

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,31 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- *
- */
-
-int main(void)
-{
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
-}
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+	print(f"{number:d} is positive")
+elif number == 0:
+	print(f"{number:d} is zero")
+else:
+	print(f"{number:d} is negative")
