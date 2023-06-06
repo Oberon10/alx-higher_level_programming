92920967f978e06c7e0d4704e38a504b9b8540c7
oberon10 <prosperbernrad@gmail.com>
2023-06-06 16:26:39 -0700

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,35 +1,11 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
-* main - Prints the last digit of a randomly generated number
-*        and whether it is greater than 5, less than 6, or 0.
-*
-* Return: Always 0.
-*/
-
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
-	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",
-			n, n % 10);
-	}
-
-	return (0);
-}
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+    print(f"last digit of {number:d} is {digit:d} and is ", end="")
+elif digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
