d35697e3bdc918546efb67cd552402280a54368b
oberon10 <prosperbernrad@gmail.com>
2023-06-06 17:39:36 -0700

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Author : Benard Prosper Echebiri
 for letter in range(97, 123):
-    if char(letter) ! = 'q' and char(letter) ! = 'e':
+    if char(letter) != 'q' and char(letter) != 'e':
         print("{}".format(chr(letter)), end="")
