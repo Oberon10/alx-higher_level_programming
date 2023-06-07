6275dbb8a67e31d82df0428e82e3479fe117f2e2
oberon10 <prosperbernrad@gmail.com>
2023-06-07 15:23:08 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-uppercase("best")
-uppercase("Best School 98 Battery street")
+    for i in range(len(str)):
+        char = ord(str[i])
+        if char >= 97 and char <= 122:
+            char = char - 32
+        print("{}".format(chr(char)), end="")
+    print()    
