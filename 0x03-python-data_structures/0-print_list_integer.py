e51e570aa82cb4dee9abb0158f76ff809b2d3579
oberon10 <prosperbernrad@gmail.com>
2023-06-11 18:32:11 -0700

    task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Authur:  Benard Prosper Echebiri
+
+def print_list_integer(my_list=[]):
+    """This print integer in the list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
