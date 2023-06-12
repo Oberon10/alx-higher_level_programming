86b4eb0ae038039b9ef1b799fcec25370051cc5a
oberon10 <prosperbernrad@gmail.com>
2023-06-12 08:38:25 -0700

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Author: Benard Prosper Echebiri
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
+    x = my_list[0]
+    for i in my_list:
+        if i > x:
+            x = i
+    return (x)
