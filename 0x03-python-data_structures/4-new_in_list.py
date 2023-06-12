146d45bd523df989fb65a3495a05f0f8abaf8091
oberon10 <prosperbernrad@gmail.com>
2023-06-12 07:45:41 -0700

    task 4 completed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,9 @@
 # Author: Benard Prosper Echebiri
 
 def new_in_list(my_list, idx, element):
-    tmp_list = my_list[:]
-    if 0 <= idx < len(my_list):
-        tmp_list[idx] = element
-        return(tmp_list)
-    return(my_list)
+    """Modifying element in a list by replacing it."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
