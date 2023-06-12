9c08938fac0ff7b119ac4b312748c2676eb0aa16
Oberon10 <125618397+Oberon10@users.noreply.github.com>
2023-06-12 02:03:31 +0100

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/1-element_at.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# Author: Benard Prosper Echebiri
-
-def element_at(my_list, idx):
-    """This will retieve element from the lis."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return None
-    return (my_list[idx])
