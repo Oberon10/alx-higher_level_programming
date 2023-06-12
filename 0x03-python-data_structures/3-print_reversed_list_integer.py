470a8dce0e9b08cc29d774204ecc43ae498304a9
oberon10 <prosperbernrad@gmail.com>
2023-06-12 08:20:42 -0700

    Task 4 completed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# Authur: Beanrd Prosper Echebiri
+# Author: Bernard Prosper Echebiri
 
 def print_reversed_list_integer(my_list=[]):
-    """Print all integer of a list in reversed order."""
-    if list isinstance(my_list, list):
-        my_list.reversed()
-        for i in my_list:
-            print("{:d}".format(i))
+    if my_list is None:
+        return
+    for i in reversed(my_list):
+        print('{:d}'.format(i))
