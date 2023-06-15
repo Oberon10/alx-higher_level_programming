c921fa525bed0081658e90bb7549271ae8755ad3
oberon10 <prosperbernrad@gmail.com>
2023-06-14 19:12:09 -0700

    task completd

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """This function modifies a values in a dictionary"""
+    a_dictionary[key] = value
+    return (a_dictionary)
