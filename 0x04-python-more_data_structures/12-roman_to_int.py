be91d2bd66531d8b06417af20fb625da0a6bbe0b
oberon10 <prosperbernrad@gmail.com>
2023-06-14 21:52:49 -0700

    task completed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-
-def roman_to_int(roman_string):
+def to_subtract(list_num):
     to_sub = 0
-    max_list = max(list_number)
+    max_list = max(list_num)
 
-    for k in list_num:
-    if max_list > k
-        to_sub += k
+    for a in list_num:
+        if max_list > a:
+            to_sub += a
+
     return (max_list - to_sub)
 
-def roman_to_int(roman_string)
-   """Tis function converts a Roman numberal to an integer"""
+
+def roman_to_int(roman_string):
     if not roman_string:
         return 0
-    if not isinstance(roman_string, str):
 
-    rom_n = {'l': 1, 'V': 5, 'X': 10, 'L': 50 ,'C':100 ,'D': 500 ,'M': 1000}
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
 
-    number = 0
+    num = 0
     last_rom = 0
-    list_number = [0]
+    list_num = [0]
 
-    for ch in roman_string:
-        for r_number in list_keys:
-            if r_num == ch:
-                if rom_n.get(ch) <= last_rom:
-                    number += to_subtract(list_number)
-                    list_number = [rom_n.get(ch)]
+    for char in roman_string:
+        for r_num in list_keys:
+            if r_num == char:
+                if rom_n.get(char) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(char)]
                 else:
-                    list_number.append(rom_n.get(ch))
-                last_rom = rom_n.get(ch)
-    number += to_subtract(list_number)
+                    list_num.append(rom_n.get(char))
 
-    return (number)
-    
+                last_rom = rom_n.get(char)
+
+    num += to_subtract(list_num)
+
+    return (num)
