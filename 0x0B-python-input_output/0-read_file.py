741c2fbd11f083b1c3de0a4a1f8c261385167a85
oberon10 <prosperbernrad@gmail.com>
2023-07-11 15:59:44 -0700

    task completed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-"""
-0-read_file module
-"""
-
-
 def read_file(filename=""):
-    """
-    read_file - reads a text file (UTF8) and prints it to stdout
-    Args:
-        filename: name of the file
-    """
-    with open(filename, "r", encoding="UTF-8") as f:
-        for line in f:
-            print(line, end="")
+    """ function that reads a text file (UTF8) and prints it to stdout """
+
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
