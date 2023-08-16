e2b9b9be19763f4543ded446bc6433c529370284
oberon10 <prosperbernrad@gmail.com>
2023-08-15 18:46:01 -0700

    task completed

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- This Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+INSERT INTO `first_table`(`id`, `name`) VALUES(89, "Best School");
