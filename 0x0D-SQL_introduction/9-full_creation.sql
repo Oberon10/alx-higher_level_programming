a00f8d7fa849cd988b05f09726a4893d69a349bf
oberon10 <prosperbernrad@gmail.com>
2023-08-15 18:43:32 -0700

    task completed

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,6 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- Creates and fills a table second_table with attributes id, name and score.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES(1, "John", 10);
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES(2, "Alex", 3);
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES(3, "Bob", 14);
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES(4, "George", 8);
