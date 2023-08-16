d67dca182ddad7946341625ae4d72642567afb84
oberon10 <prosperbernrad@gmail.com>
2023-08-16 04:54:26 -0700

    task completed

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
--- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
--- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
-        INNER JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Displays NULL for shows without genres.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.title, g.genre_id FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g
+ON s.id = g.show_id
+ORDER BY s.title, g.genre_id
