8b0b30e069226f503de6e69b378b8845c6431dba
oberon10 <prosperbernrad@gmail.com>
2023-08-15 18:39:58 -0700

    task completed

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,2 @@
--- lists all rows of the table
-SELECT COUNT(*)
-FROM `first_table`
-WHERE `id` = 89;
+-- This Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
