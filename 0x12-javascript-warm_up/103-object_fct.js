4aac1e085a465421962fb2254455c5e787fdb0ba
oberon10 <prosperbernrad@gmail.com>
2023-09-12 16:57:54 -0700

    task completed

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
-const myObject = {
+let myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-/*
-YOUR CODE HERE
-*/
+
+myObject.incr = function () {
+  this.value++;
+};
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
-ctestonsole.log(myObject);
+console.log(myObject);
